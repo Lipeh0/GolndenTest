e982981af58c8ca77c73210781c4781418d038c9
Projects <102307074+Lipeh0@users.noreply.github.com>
2025-05-07 17:10:57 -0300

    Add files via upload

---
+++ GoldenRazor/js/script.js
@@ -0,0 +1,27 @@
+function toggleMenu() {
+    const sidebar = document.getElementById("sidebar");
+    if (sidebar.style.left === "0px") {
+        sidebar.style.left = "-250px";
+    } else {
+        sidebar.style.left = "0px"; 
+    }
+}
+
+
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
